b567f190e799868c0692ee6d770db9c68e2472be
PaulaEs <paulaproxy@gmail.com>
2013-12-21 23:57:20 +0100

    Email address can't be blank.

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:email_address) { EmailAddress.new(address: 'example@example.com') }
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without an address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
 end
