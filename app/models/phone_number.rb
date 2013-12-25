eac40fe1640fe71f5531bcd51b63c5426c7f84b7
PaulaEs <paulaproxy@gmail.com>
2013-12-25 23:49:11 +0100

    Fixing phone number model specs.

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,5 +1,4 @@
 class PhoneNumber < ActiveRecord::Base
-  belongs_to :person
-  belongs_to :company
-  validates :number, :person_id, presence: true
+  belongs_to :contact, polymorphic: true
+  validates :number, :contact_id, presence: true
 end
