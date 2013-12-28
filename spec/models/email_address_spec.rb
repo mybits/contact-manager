0b71527e54f99ea3f3c69c604483dd3fca1555e4
PaulaEs <paulaproxy@gmail.com>
2013-12-28 22:01:51 +0100

    Fixing email address model specs.

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe EmailAddress do
 
-  let(:email_address) { EmailAddress.new(address: 'example@example.com', person_id: 1) }
+  let(:email_address) { EmailAddress.new(address: 'example@example.com', contact_id: 1, contact_type: 'Person') }
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -13,9 +13,12 @@ describe EmailAddress do
     expect(email_address).to_not be_valid
   end
 
-  it 'must has a reference to a person' do
-    email_address.person_id = nil
+  it 'must be linked to a contact' do
+    email_address.contact_id = nil
     expect(email_address).to_not be_valid
   end
 
+  it 'is associated with a person' do
+    expect(email_address).to respond_to(:contact)
+  end
 end
