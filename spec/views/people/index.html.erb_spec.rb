4c289fa66cb5e612126afe2df6f14c8084bf3809
PaulaEs <paulaproxy@gmail.com>
2013-12-31 02:12:22 +0100

    Improving index views.

--- spec/views/people/index.html.erb_spec.rb
+++ spec/views/people/index.html.erb_spec.rb
@@ -4,12 +4,10 @@ describe "people/index" do
   before(:each) do
     assign(:people, [
       stub_model(Person,
-        :first_name => "First Name",
-        :last_name => "Last Name"
+        :first_name => "Name",
       ),
       stub_model(Person,
-        :first_name => "First Name",
-        :last_name => "Last Name"
+        :last_name => "Name"
       )
     ])
   end
@@ -17,7 +15,6 @@ describe "people/index" do
   it "renders a list of people" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
   end
 end
