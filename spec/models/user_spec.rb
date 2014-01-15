550e25bca978c31890896b49680f3b6890280c6b
PaulaEs <paulaproxy@gmail.com>
2014-01-15 01:46:47 +0100

    Testing people get associated with user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,14 @@ describe User do
   it 'has associated people' do
     expect(user.people).to be_instance_of(ActiveRecord::Associations::CollectionProxy::ActiveRecord_Associations_CollectionProxy_Person)
   end
+
+  it 'builds associated people' do
+    person_1 = Fabricate(:person)
+    person_2 = Fabricate(:person)
+    [person_1, person_2].each do |person|
+      expect(user.people).to_not include(person)
+      user.people << person
+      expect(user.people).to include(person)
+    end
+  end
 end
