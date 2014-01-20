405883fed5caca4b7767fd3b839b5a0040c7f88f
PaulaEs <paulaproxy@gmail.com>
2014-01-20 02:01:38 +0100

    Omni typo.

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -1,6 +1,6 @@
 def login_as(user)
-  Omniauth.config.test_mode = true
-  Omniauth.config.mock_auth[:twitter] = {
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:twitter] = {
     "provider" => user.provider,
     "uid" => user.uid,
     "info" => {"name" => user.name}
